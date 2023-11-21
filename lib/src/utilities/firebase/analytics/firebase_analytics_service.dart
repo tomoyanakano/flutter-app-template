bc3d2d020ed6e8661c3d374c4f5d00d68a5893dd
tomoyanakano <tomo1999724@gmail.com>
2023-11-21 20:00:09 +0900

    :heavy_plus_sign: Add firebase_analytics

---
+++ lib/src/utilities/firebase/analytics/firebase_analytics_service.dart
@@ -0,0 +1,70 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+part 'firebase_analytics_service.g.dart';
+
+class FirebaseAnalyticsService {
+  FirebaseAnalyticsService(this.ref, this.instance);
+  final Ref ref;
+  final FirebaseAnalytics instance;
+
+  /// log event
+  Future<void> logEvent(
+      {required String name, Map<String, dynamic>? parameters}) async {
+    await instance.logEvent(name: name, parameters: parameters);
+  }
+
+  late FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: instance);
+
+  /// log app open event
+  void setUserProperties({
+    required String name,
+    required String value,
+  }) {
+    instance.setUserProperty(name: name, value: value);
+  }
+
+  /// set current user id
+  void setUserId(String id) => instance.setUserId(id: id);
+
+  /// set current screen
+  void setCurrentScreen(String screenName) =>
+      instance.setCurrentScreen(screenName: screenName);
+
+  /// log search event
+  void logSearch(String keyword) => instance.logSearch(searchTerm: keyword);
+
+  /// log view search results event
+  void logViewSearchResults(String keyword) =>
+      instance.logViewSearchResults(searchTerm: keyword);
+
+  /// log login event
+  void logLogin(String loginMethod) =>
+      instance.logLogin(loginMethod: loginMethod);
+
+  /// log sign up event
+  void logSignUp(String signUpMethod) =>
+      instance.logSignUp(signUpMethod: signUpMethod);
+
+  /// log tutorial begin event
+  void logTutorialBegin() => instance.logTutorialBegin();
+
+  /// log tutorial complete event
+  void logTutorialComplete() => instance.logTutorialComplete();
+
+  /// コンテンツの選択 ※購買行動にあたる商品選択は下記の selectItem を使用する
+  void logSelectContent({
+    required String contentType,
+    required String itemId,
+  }) =>
+      instance.logSelectContent(
+        contentType: contentType,
+        itemId: itemId,
+      );
+}
+
+@riverpod
+FirebaseAnalyticsService firebaseAnalyticsService(Ref ref) =>
+    FirebaseAnalyticsService(ref, FirebaseAnalytics.instance);
---
+++ lib/src/utilities/firebase/analytics/firebase_analytics_service.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_analytics_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAnalyticsServiceHash() =>
+    r'820405ce0b2b3f606e7587e3d72f6105f2c56913';
+
+/// See also [firebaseAnalyticsService].
+@ProviderFor(firebaseAnalyticsService)
+final firebaseAnalyticsServiceProvider =
+    AutoDisposeProvider<FirebaseAnalyticsService>.internal(
+  firebaseAnalyticsService,
+  name: r'firebaseAnalyticsServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseAnalyticsServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseAnalyticsServiceRef
+    = AutoDisposeProviderRef<FirebaseAnalyticsService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
